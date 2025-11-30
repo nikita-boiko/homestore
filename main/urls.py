d64d4f58d8421fcf727870b6743dfbfcac49c3ce
nikita-boiko <nik.bojko.02@mail.ru>
2025-11-30 18:20:11 +0300

    The contacts and deliver are ready

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,6 @@ app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('deliver/', views.deliver, name='deliver'),
 ]
--- main/views.py
+++ main/views.py
@@ -22,3 +22,25 @@ def about(request):
 
 
     return render(request, 'main/about.html', context)
+
+def contacts(request):
+    context = {
+        'title': 'Home - Контактная ифнормация',
+        'content': 'Контактная ифнормация',
+        'text_on_page': 'Järve tornid, Järve tn 2, Таллинн, 11314. Часы работы салона: Пн-Пт 9-18, Сб 10-15.Тел.: +372 601 1441 Моб.: +372 51 999 622Эл. почта: info@alexanto.ee'
+        
+    }
+
+
+    return render(request, 'main/contacts.html', context)
+
+def deliver(request):
+    context = {
+        'title': 'Home - Доставка и оплата',
+        'content': 'Доставка и оплата',
+        'delivery_info': ' Доставка: по Таллинну от 50€ бесплатно (1-2 дня), по Эстонии от 100€ бесплатно (2-3 дня). Оплата: картой онлайн, наличными/картой при получении, рассрочка 0% на 3 месяца. Самовывоз: Järve tn 2, Таллинн. Тел: +372 601 1441'
+        
+    }
+
+
+    return render(request, 'main/deliver.html', context)
