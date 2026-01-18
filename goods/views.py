fd7656295154070b950994ad2905e24fff5f1167
nikita-boiko <nik.bojko.02@mail.ru>
2026-01-18 13:57:23 +0300

    Second pagination option _ Get patams

--- goods/views.py
+++ goods/views.py
@@ -5,8 +5,8 @@ from django.template import context
 from goods.models import Products
 
 
-def catalog(request, category_slug, page=1):
-    
+def catalog(request, category_slug):
+    page  = request.GET.get('page', 1)
 
     if category_slug == "vse-tovary":
         goods = Products.objects.all()
@@ -14,7 +14,7 @@ def catalog(request, category_slug, page=1):
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
     
     
     context = {
