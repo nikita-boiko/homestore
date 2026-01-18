0e65c31ced104acbf79362fc233fb16a7353a604
nikita-boiko <nik.bojko.02@mail.ru>
2026-01-18 21:26:51 +0300

    The product filter on the site page and urlencode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,4 +1,5 @@
 
+from django.utils.http import urlencode
 from goods.models import Categories
 
 from django import template
@@ -8,3 +9,9 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query= context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
